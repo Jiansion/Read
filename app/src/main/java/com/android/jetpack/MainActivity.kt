f32d80965473724e8484851d1ebaa86d1013c9a0
YeJian <amazeconch@gmail.com>
2019-11-13 17:10:46 +0800

    init project

---
+++ app/src/main/java/com/android/jetpack/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.android.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if(savedInstanceState == null){
+//            supportFragmentManager.beginTransaction()
+//                .add(R.id.fragment_container,)
+        }
+    }
+}
---
+++ app/src/main/java/com/android/jetpack/ui/ProductListFragment.kt
@@ -0,0 +1,24 @@
+package com.android.jetpack.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * @author: YeJian
+ * @email: amazeconch@gmail.com
+ * Create on: 2019/11/13
+ * Instructions:
+ **/
+class ProductListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
